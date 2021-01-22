b182e4d3d163f551b4f95fc16ba187ab0db8d2fc
Andrew Herbig <notandrewherbig@gmail.com>
2021-01-22 17:58:00 -0500

    if urls

--- updatehook.py
+++ updatehook.py
@@ -26,18 +26,19 @@ async def main():
             img.attrib["data-original"] for img in html.cssselect("div.news_box img")
         ]
 
-        for url in urls:
-            if WEBHOOK and url not in all_urls:
-                print(url)
-                all_urls.append(url)
-                async with session.get(url) as response:
-                    image_data = await response.read()
-                data = aiohttp.FormData()
-                data.add_field("file", image_data, filename=URL(url).name)
-                await session.post(WEBHOOK, data=data)
+        if urls:
+            for url in urls:
+                if WEBHOOK and url not in all_urls:
+                    print(url)
+                    all_urls.append(url)
+                    async with session.get(url) as response:
+                        image_data = await response.read()
+                    data = aiohttp.FormData()
+                    data.add_field("file", image_data, filename=URL(url).name)
+                    await session.post(WEBHOOK, data=data)
 
-    ALL_URLS.write_text("".join(f"{url}\n" for url in all_urls))
-    CURRENT_URLS.write_text("".join(f"{url}\n" for url in urls))
+            ALL_URLS.write_text("".join(f"{url}\n" for url in all_urls))
+            CURRENT_URLS.write_text("".join(f"{url}\n" for url in urls))
 
 
 if __name__ == "__main__":
